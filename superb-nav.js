261b6dac9737c6daa4f95ee86c8df14b6d0b46ad
Burger Vivier <85217933+BoksBurger@users.noreply.github.com>
2022-08-10 12:52:43 +0200

    Added image preloading option
    Apart from adding the option to preload images, I refactored the code for better readability.

--- superb-nav.js
+++ superb-nav.js
@@ -1,71 +1,86 @@
 //**** This is the plugin part ****//
 let currentImage = 0;
-Object.prototype.makeSuperb = function(options){
-    options["container"] =  this.id;
-    superbImageNav(options); 
-}
-function superbImageNav(options) {
-    if (options?.imgList.length > -1) {
-        if (!options?.container) console.log("Please specify the container element ID");
-        let container = document.getElementById(options?.container);
-        let viewPort = document.createElement("div");
-        let hint = document.createElement("div");
-        viewPort.className = "imgViewer";
-        container.className = "imgNav";
-        container.appendChild(viewPort);
+Object.prototype.makeSuperb = function (options) {
+  options["container"] = this.id;
+  if (!Array.isArray(options.imgList))
+    throw new Error("Please specify a String[] of image URLs");
 
-        viewPort.style.width = `${options.width ? options.width : 300}px`;
-        viewPort.style.height = `${options.height ? options.height : 300}px`;
-        viewPort.style.borderRadius = `${options.roundCorners ? options.roundCorners : 0}px`;
-        viewPort.style.boxShadow = `${options.dropShaddow ? "1px 1px 5px black" : "unset"}`;
+  if (options.imgList.length > 0) {
+    let container = document.getElementById(options?.container);
+    let image = document.createElement("div");
+    let hint = document.createElement("div");
+    let width = `${options.width ? options.width : 300}px`;
+    let height = `${options.height ? options.height : 300}px`;
+    let radius = `${options.roundCorners ? options.roundCorners : 0}px`;
+    let shaddow = `${options.dropShaddow ? "1px 1px 5px black" : "unset"}`;
+    let hintText = "Use the j/k or arrow keys to navigate between images.";
 
-        if (options.showHint) {
-            hint.className = "hintText"
-            hint.innerText = options.hintText ? options.hintText : "Use the j/k or arrow keys to navigate between images.";
-            hint.style.width = `${options.width ? options.width : 300}px`
-            hint.style.borderRadius = `${options.roundCorners ? options.roundCorners : 0}px`;
-            hint.style.boxShadow = `${options.dropShaddow ? "1px 1px 5px black" : "unset"}`
-            container.appendChild(hint);
-        }
-        function updateImage(i) {
-            viewPort.style.backgroundImage = `url(${options.imgList[i]})`
-        }
+    container.className = "imgNav";
+    image.className = "imgViewer";
 
-        if (options.slideShow) {
-            setInterval(() => {
-                options.reverseShow ? previousImage() : nextImage()
-            }, options.slideShow * 1000);
-        }
+    image.style.width = width;
+    image.style.height = height;
+    image.style.borderRadius = radius;
+    image.style.boxShadow = shaddow;
+    container.appendChild(image);
 
-        //Capture keyboard event
-        document.onkeydown = (e) => {
-            if (e.key === "k" || e.key === "K" || e.key === "ArrowRight") {
-                nextImage();
-            } else if (e.key === "j" || e.key === "J" || e.key === "ArrowLeft") {
-                previousImage();
-            }
-            updateImage(currentImage);
-        };
-
-        function nextImage() {
-            if (currentImage < options.imgList.length - 1) {
-                currentImage++;
-            } else if (options.loopImages) {
-                currentImage = 0;
-            }
-            updateImage(currentImage);
-        }
-
-        function previousImage() {
-            if (currentImage > 0) {
-                currentImage--;
-            } else if (options.loopImages) {
-                currentImage = options.imgList.length - 1;
-            }
-            updateImage(currentImage);
-        }
-        //Set initial image
-        updateImage(currentImage);
+    if (options.showHint) {
+      hint.className = "hintText";
+      hint.innerText = options.hintText ? options.hintText : hintText;
+      hint.style.width = width;
+      hint.style.borderRadius = radius;
+      hint.style.boxShadow = shaddow;
+      container.appendChild(hint);
     }
-}
+
+    if (options.slideShow) {
+      setInterval(() => {
+        options.reverseShow ? previousImage() : nextImage();
+      }, options.slideShow * 1000);
+    }
+
+    if (options.preloadImages) {
+      for (let i = 0; i < options.imgList.length; i++) {
+        const imageURL = options.imgList[i];
+        const img = new Image();
+        img.src = imageURL;
+      }
+    }
+
+    function updateImage(i) {
+      image.style.backgroundImage = `url(${options.imgList[i]})`;
+    }
+
+    document.onkeydown = (e) => {
+      if (e.key === "k" || e.key === "K" || e.key === "ArrowRight") {
+        nextImage();
+      }
+      if (e.key === "j" || e.key === "J" || e.key === "ArrowLeft") {
+        previousImage();
+      }
+    };
+
+    function nextImage() {
+      if (currentImage < options.imgList.length - 1) {
+        currentImage++;
+      } else if (options.loopImages) {
+        currentImage = 0;
+      }
+      updateImage(currentImage);
+    }
+
+    function previousImage() {
+      if (currentImage > 0) {
+        currentImage--;
+      } else if (options.loopImages) {
+        currentImage = options.imgList.length - 1;
+      }
+      updateImage(currentImage);
+    }
+    //Set initial image
+    updateImage(currentImage);
+  } else {
+    throw new Error("Image list is empty. Add URLs to 'imgList'.");
+  }
+};
 //**** End of the plugin part ****//
